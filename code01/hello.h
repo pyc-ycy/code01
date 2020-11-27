1d9ec37ab6df764e58a79c931f3877cd0921eb6e
7wc98#14 <2923616405@qq.com>
2020-11-27 16:01:34 +0800

    moodify

--- code01/hello.h
+++ code01/hello.h
@@ -7,3 +7,29 @@ int hello(void)
 
 	return 12;
 }
+
+void formatIO(void)
+{
+	int num1 = 1024;
+	long num2 = 655356;
+	int num3 = -15;
+	int a = 65;
+	float num4 = 123.456;
+	char str[24] = "Hello world\0";
+	// 首先体验%d格式输出
+	printf("num1=%d,num4=%d\n", num1, num4);
+	//%ld
+	printf("num2=%ld\n", num2);
+	// %md
+	printf("num1=%5d, num2=%5d\n", num1, num2);
+	// %u
+	printf("num3=%d,num3=%u\n", num3, num3);
+	//%f %lf
+	printf("num4=%f,num4=%.1f\n", num4, num4);
+	// %c
+	printf("a=%d, a=%c\n");
+	// %s
+	printf("str=%s\n", str);
+	//%o %x
+	printf("num2=%o, num4=%x", num2, num4);
+}
