2996da272aa1b153319d9aa38519d9aad357b2c6
7wc98#14 <2923616405@qq.com>
2020-11-30 21:21:09 +0800

    modify

--- code01/AboutVariable.h
+++ code01/AboutVariable.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <iomanip>
+#include <stdio.h>
 using namespace std;
 
 //void testRubbishDatum(void)
@@ -7,3 +9,15 @@ using namespace std;
 //	int a;
 //	cout << "未初始化的变量a的内容：" << a << endl;
 //}
+
+void templateOfUsingDataType(void)
+{
+	int a;
+	cin >> a;
+	int b = 1456;
+	printf("a=%d\n", a);
+	cout.width(6);
+	cout.fill('#');
+	cout << setiosflags(ios::right);
+	cout << b << endl;
+}
--- code01/code01.cpp
+++ code01/code01.cpp
@@ -6,7 +6,7 @@
 #include "AboutVariable.h"
 int main()
 {
-	testRubbishDatum();
+	templateOfUsingDataType();
 	return 0;
 }
 
